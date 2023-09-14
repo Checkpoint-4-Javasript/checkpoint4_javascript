12e2668ac489d39fba74c0f629530df622837f4d
Gabriel Rocha <shadowninja2093@gmail.com>
2023-09-14 10:05:49 -0300

    Trocando as imagens para uma só

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -12,21 +12,21 @@ function Produtos(){
         </figcaption>
 
         <figure>
-            <img src="http://lorempixel.com.br/450/200/?1" alt="Lorem ipsum image" />
+            <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" alt="Lorem ipsum image" />
         </figure>
         <figcaption>
             <p>Lorem ipsum</p>
         </figcaption>
 
         <figure>
-            <img src="http://lorempixel.com.br/450/200/?2" alt="Lorem ipsum image" />
+            <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" alt="Lorem ipsum image" />
         </figure>
         <figcaption>
             <p>Lorem ipsum</p>
         </figcaption>
 
         <figure>
-            <img src="http://lorempixel.com.br/450/200/?3" alt="Lorem ipsum image" />
+            <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" alt="Lorem ipsum image" />
         </figure>
         <figcaption>
             <p>Lorem ipsum</p>
