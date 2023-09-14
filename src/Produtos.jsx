438d19258ed41f02e0bf5befae113e65ec242d9e
Gabriel Rocha <shadowninja2093@gmail.com>
2023-09-14 10:22:58 -0300

    Colocando imagens únicas

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -12,21 +12,21 @@ function Produtos(){
         </figcaption>
 
         <figure>
-            <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" alt="Lorem ipsum image" />
+            <img src="https://fastly.picsum.photos/id/888/200/300.jpg?hmac=WJcjbSQSOOzfBpPsUKZHZzjBCJjDv43RwJ8P8Ag2xj4" alt="Lorem ipsum image" />
         </figure>
         <figcaption>
             <p>Lorem ipsum</p>
         </figcaption>
 
         <figure>
-            <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" alt="Lorem ipsum image" />
+            <img src="https://fastly.picsum.photos/id/573/200/300.jpg?hmac=3xlTAEXK1dTOXmwZnXwdOV0tTFX1rbg_wL_cava2HfE" alt="Lorem ipsum image" />
         </figure>
         <figcaption>
             <p>Lorem ipsum</p>
         </figcaption>
 
         <figure>
-            <img src="https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" alt="Lorem ipsum image" />
+            <img src="https://fastly.picsum.photos/id/671/200/300.jpg?hmac=I6A0nYPaPDSVj8NEal4lbRDDN6E617I8S8Pc3ulWHS8" alt="Lorem ipsum image" />
         </figure>
         <figcaption>
             <p>Lorem ipsum</p>
